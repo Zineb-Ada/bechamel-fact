33f5dc121f0628f66bb085af4cf024ee03c0ece2
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2022-04-21 16:01:35 +0200

    Equaf.compare & String.compare done

--- test-bechamel/_build/default/fact.ml
+++ test-bechamel/_build/default/fact.ml
@@ -1,60 +1,66 @@
+external random_seed : unit -> int array = "caml_sys_random_seed"
+let seed = "4EygbdYh+v35vvrmD9YYP4byT5E3H7lTeXJiIj+dQnc="
+let seed = Base64.decode_exn seed
+let seed =
+   let res = Array.make (String.length seed / 2) 0 in
+   for i = 0 to (String.length seed / 2) - 1
+   do res.(i) <- (Char.code seed.[i * 2] lsl 8) lor (Char.code seed.[i * 2 + 1]) done ;
+   res
+
+let () =
+  let random_seed = seed in
+  Random.full_init random_seed
+
+let random length =
+  let get _ =
+    match Random.int (10 + 26 + 26) with
+    | n when n < 10 -> Char.(chr (code '0' + n))
+    | n when n < 10 + 26 -> Char.(chr (code 'a' + n - 10))
+    | n -> Char.(chr (code 'A' + n - 10 - 26)) in
+  String.init length get
+
+
 open Bechamel
 open Toolkit
 
-let () = Random.self_init ()
+let hash_eq_0 = random 4096
+let hash_eq_1 = Bytes.to_string (Bytes.of_string hash_eq_0)
 
-let imp_fact x =
-  let y = ref 0 in
-  let r = ref 1 in
-  while !y < x do
-    y := !y + 1;
-    r := !r * !y
-  done;
-  !r
+let hash_neq_0 = random 4096
+let hash_neq_1 =
+  let rec go limit =
+    if limit <= 0 then failwith "Impossible to generate different hashes." ;
+    let res = random 4096 in
+    if res = hash_neq_0 then go (pred limit) else res in
+  go 10
 
-let rec fun_fact x = if x = 0 then 1 else x * fun_fact (x - 1)
-let random_max = 32767.
-let ( <.> ) f g x = f (g x)
+(* let ( <.> ) *)
+let teste0 =
+  Test.make ~name:"eqaf equal equal" (Staged.stage @@ fun () -> Eqaf.equal hash_eq_0 hash_eq_1)
+let teste1 =
+  Test.make ~name:"eqaf equal not equal" (Staged.stage @@ fun () -> Eqaf.equal hash_neq_0 hash_neq_1)
 
-let normal n =
-  let m = n + (n mod 2) in
-  let values = Array.create_float m in
-  for i = 0 to (m / 2) - 1 do
-    let x = ref 0. and y = ref 0. and rsq = ref 0. in
-    while
-      x := (Random.float random_max /. random_max *. 2.0) -. 1.;
-      y := (Random.float random_max /. random_max *. 2.0) -. 1.;
-      rsq := (!x *. !x) +. (!y *. !y);
-      !rsq >= 1. || !rsq = 0.
-    do
-      ()
-    done;
-    let f = sqrt (-2.0 *. log !rsq /. !rsq) in
-    values.(i * 2) <- !x *. f;
-    values.((i * 2) + 1) <- !y *. f
-  done;
-  Array.map (Float.to_int <.> ( *. ) random_max) values
+let teste2 =
+  Test.make ~name:"string equal equal" (Staged.stage @@ fun () -> String.equal hash_eq_0 hash_eq_1)
 
-let imp_fact n =
-  let vs = normal n in
-  Staged.stage @@ fun () -> Array.iter (ignore <.> imp_fact <.> abs) vs
+let teste3 =
+  Test.make ~name:"string equal not equal" (Staged.stage @@ fun () -> String.equal hash_neq_0 hash_neq_1)
 
-let fun_fact n =
-  let vs = normal n in
-  Staged.stage @@ fun () -> Array.iter (ignore <.> fun_fact <.> abs) vs
+let testc0 =
+  Test.make ~name:"eqaf compare equal" (Staged.stage @@ fun () -> Eqaf.compare_be hash_eq_0 hash_eq_1)
+let testc1 =
+  Test.make ~name:"eqaf compare not equal" (Staged.stage @@ fun () -> Eqaf.compare_be hash_neq_0 hash_neq_1)
 
-let test0 =
-  Test.make_indexed ~name:"functional" ~fmt:"%s %d" ~args:[ 10; 50; 100 ]
-    fun_fact
+let testc2 =
+  Test.make ~name:"string compare equal" (Staged.stage @@ fun () -> String.compare hash_eq_0 hash_eq_1)
 
-let test1 =
-  Test.make_indexed ~name:"imperative" ~fmt:"%s %d" ~args:[ 10; 50; 100 ]
-    imp_fact
+let testc3 =
+  Test.make ~name:"string compare not equal" (Staged.stage @@ fun () -> String.compare hash_neq_0 hash_neq_1)
 
 let benchmark () =
   let ols =
-    Analyze.ols ~bootstrap:0 ~r_square:true ~predictors:Measure.[| run |]
-  in
+    Analyze.ols ~bootstrap:0 ~r_square:true ~predictors:Measure.[| run |] in
+
   let instances =
     Instance.[ minor_allocated; major_allocated; monotonic_clock ]
   in
@@ -62,9 +68,15 @@ let benchmark () =
     Benchmark.cfg ~limit:2000 ~stabilize:true ~quota:(Time.second 0.5)
       ~kde:(Some 1000) ()
   in
+  (* let raw_results =
+    Benchmark.all cfg instances
+      
+  in *)
   let raw_results =
     Benchmark.all cfg instances
-      (Test.make_grouped ~name:"factorial" ~fmt:"%s %s" [ test0; test1 ])
+      (* (Test.make_grouped ~name:"compare" ~fmt:"%s %s" [ teste0; teste1; teste2; teste3 ]) *)
+      (Test.make_grouped ~name:"compare" ~fmt:"%s %s" [ testc0; testc1; testc2; testc3;teste0; teste1; teste2; teste3 ])
+      
   in
   let results =
     List.map (fun instance -> Analyze.all ols instance raw_results) instances
@@ -72,19 +84,13 @@ let benchmark () =
   let results = Analyze.merge ols instances results in
   (results, raw_results)
 
-let compare k0 k1 =
-  let a = ref 0 and b = ref 0 in
-  Scanf.sscanf k0 "%s %s %d" (fun _ _ a' -> a := a');
-  Scanf.sscanf k1 "%s %s %d" (fun _ _ b' -> b := b');
-  !a - !b
-
 let nothing _ = Ok ()
 
 let () =
   let results = benchmark () in
   let results =
     let open Bechamel_js in
-    emit ~dst:(Channel stdout) nothing ~compare ~x_label:Measure.run
+    emit ~dst:(Channel stdout) nothing ~compare:String.compare ~x_label:Measure.run
       ~y_label:(Measure.label Instance.monotonic_clock)
       results
   in
